9571203ad4c9fedc7bd25b7fb3a1ce37f4f55527
Danilo Donato <51414398+danilo8br@users.noreply.github.com>
2021-01-20 13:19:09 -0300

    Update wiki.py

--- wiki.py
+++ wiki.py
@@ -1,8 +1,9 @@
+# Importando bibliotecas 
 from PyQt5 import uic, QtWidgets
 import wikipedia
-
+# Conectando a aplicação
 app=QtWidgets.QApplication([])
-
+# Buscando as informações
 def wiki():
     wikipedia.set_lang('pt')
     pergunta = str(primeira_tela.lineEdit.text())
@@ -11,12 +12,12 @@ def wiki():
     segunda_tela.show()
     segunda_tela.label_2.setText(n)
 
-
+# Carregando os arquivos
 primeira_tela = uic.loadUi('primeira_tela.ui')
 segunda_tela = uic.loadUi('segunda_tela.ui')
-
+# Botões
 primeira_tela.pushButton.clicked.connect(wiki)
-
+# Mostrando a primeira tela
 primeira_tela.show()
-
+# Executando a aplicação 
 app.exec()
